9d050af3d2da8c9aed4fd353e64f2331be1dcd7c
DevNandiniGupta <145434039+DevNandiniGupta@users.noreply.github.com>
2024-04-25 21:59:20 +0530

    Add files via upload

---
+++ src/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideBar from '../Components/SideBar';
+
+const Home = () => {
+  return (
+    <div style={{ display: 'flex' }}>
+      <SideBar />
+    </div>
+  );
+};
+
+export default Home;
